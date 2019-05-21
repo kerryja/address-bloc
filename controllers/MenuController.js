765f9cc84e30a8b2e595907f56c138499fdd63d8
Kerry Jackson <kerry.jackson108@gmail.com>
2019-05-21 13:12:58 -0400

    Assignment 2963: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,6 +9,7 @@ module.exports = class MenuController {
 				message: 'Please choose from an option below: ',
 				choices: [
 					'Add new contact',
+					'Get date',
 					'Exit'
 				]
 			}
@@ -22,6 +23,10 @@ module.exports = class MenuController {
 			switch (response.mainMenuChoice) {
 				case "Add new contact":
 					this.addContact();
+					break;
+				case "Get date":
+					this.getDate();
+					this.main();
 					break;
 				case "Exit":
 					this.exit();
@@ -48,5 +53,10 @@ module.exports = class MenuController {
 	exit() {
 		console.log('Thanks for using AddressBloc!');
 		process.exit();
+	}
+
+	getDate() {
+		var today = new Date().toLocaleString();
+		console.log(today);
 	}
 }
